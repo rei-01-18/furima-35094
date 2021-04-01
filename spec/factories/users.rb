28907a0fb1f5cc701bc56615a166d06be16613fc
rei-01-18 <80232197+rei-01-18@users.noreply.github.com>
2021-04-01 12:30:35 +0900

    ユーザー管理機能　修正４

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     nickname {"test太郎"}
-    email {"aaa@aa"}
+    email {Faker::Internet.free_email}
     password {"111aaa"}
     password_confirmation {"111aaa"}
     last_name {"テスト"}
