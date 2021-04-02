37e255ba2e8581bbe728088cf804e6c87972502e
rei-01-18 <80232197+rei-01-18@users.noreply.github.com>
2021-04-02 19:24:40 +0900

    商品出品機能　修正２

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -98,6 +98,12 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not a number")
       end
+      it 'userが紐付いていないと保存できないこと' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
+      
     end
   end
 end
